d26caf325c006822a16498a01780fbc71b0d6b3c
dev-status-200 <dev.status.200@gmail.com>
2024-06-11 17:52:29 +0500

    update

--- index.js
+++ index.js
@@ -75,4 +75,4 @@ app.use("/ports", ports );
 
 const PORT = process.env.PORT || 8081; 
 
-app.listen(PORT, () => { console.log(`App listenings on port ${PORT}`) });
+app.listen(PORT, () => { console.log(`App listenings on port of Koyeb ${PORT}`) });
